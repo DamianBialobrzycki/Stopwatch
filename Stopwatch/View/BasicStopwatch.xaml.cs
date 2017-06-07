eac8cba179f1562831e37751afd607bf2c290165
Damian Białobrzycki <damianbialobrzycki@gmail.com>
2017-06-07 12:49:29 +0200

    Updated MainWindow.xaml and BasicStopwatch.xaml.cs

--- Stopwatch/View/BasicStopwatch.xaml.cs
+++ Stopwatch/View/BasicStopwatch.xaml.cs
@@ -20,9 +20,33 @@ namespace Stopwatch.View
     /// </summary>
     public partial class BasicStopwatch : UserControl
     {
+        ViewModel.StopwatchViewModel viewModel;
+
         public BasicStopwatch()
         {
             InitializeComponent();
+
+            viewModel = FindResource("viewModel") as ViewModel.StopwatchViewModel;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Start();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Stop();
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Reset();
+        }
+
+        private void LapButton_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.Lap();
         }
     }
 }
